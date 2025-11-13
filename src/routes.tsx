4034945ec147c4f6a4ebb081f2c7f684a36f2b43
GabrielPintoDev <gabrielandrade.colorado@gmail.com>
2025-11-12 21:12:13 -0300

    retirando warnings

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Loja from './pages/Loja'
 import Restaurant from './components/Restaurant'
 
 const Rotas = () => (
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle, styled } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const Cores = {
   vermelho: '#E66767',
